c5bb128523e160900c049576c925219a3e3c1541
João Victor Barbosa <joaovgb5@gmail.com>
2023-08-25 11:21:25 -0300

    initial commit

---
+++ src/components/GenerateAllButton.tsx
@@ -0,0 +1,20 @@
+import { generateCertificate } from "../utils/GenerateCertificate";
+
+interface GenerateAllButtonProps {
+  people: Person[];
+}
+
+export const GenerateAllButton = (props: GenerateAllButtonProps) => {
+  const { people } = props;
+
+  return (
+    <>
+      <button
+        className="bg-primary py-2 text-white font-bold mt-4"
+        onClick={() => people?.forEach((person) => generateCertificate(person))}
+      >
+        Baixar todos os certificados ({people?.length ?? 0})
+      </button>
+    </>
+  );
+};
