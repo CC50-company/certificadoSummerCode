4f3b3a25564fb291a3ecc5f419756363a63e3c32
João Victor Barbosa <joaovgb5@gmail.com>
2023-12-06 16:28:09 -0300

    Revert "update pdf changed"
    This reverts commit 605a443e19d779aff5e4f0c210c1172cf756d97d.

--- api/src/modules/certificate-generator/services/GenerateCertificate.ts
+++ api/src/modules/certificate-generator/services/GenerateCertificate.ts
@@ -10,7 +10,7 @@ const EMAIL_FONT_SIZE = 18;
 const ID_FONT_SIZE = 18;
 const DATE_FONT_SIZE = 15;
 const TEXT_MAX_WIDTH = 700;
-const TEXT_COLOR = rgb(0, 0, 0);
+const TEXT_COLOR = rgb(0, 0.647, 0.169);
 
 export async function createCertificate(student: Student): Promise<Uint8Array> {
   const roboto = fs.readFileSync(robotoPath);
@@ -35,21 +35,48 @@ export async function createCertificate(student: Student): Promise<Uint8Array> {
 
   const studentBoxHeight = font.heightAtSize(studentFontSize);
   pdfPage.drawText(name, {
-    y: 390 - studentBoxHeight / 2,
+    y: 339 - studentBoxHeight / 2,
     size: studentFontSize,
     font,
-    x: pdfWidth / 3.15 - studentBoxWidth / 2,
+    x: pdfWidth / 2 - studentBoxWidth / 2,
     color: TEXT_COLOR,
   });
- 
+  const emailText = email ? `Email: ${email}` : '';
+  const titleLineHeight = font.heightAtSize(EMAIL_FONT_SIZE);
+  let emailFontSize = EMAIL_FONT_SIZE + 1;
+  let titleLineWidth: number;
+  do {
+    emailFontSize -= 1;
+    titleLineWidth = font.widthOfTextAtSize(emailText, emailFontSize);
+  } while (titleLineWidth > TEXT_MAX_WIDTH);
+
+  pdfPage.drawText(emailText, {
+    y: 310 - titleLineHeight / 2,
+    size: EMAIL_FONT_SIZE,
+    font,
+    x: pdfWidth / 2 - titleLineWidth / 2,
+    color: TEXT_COLOR,
+  });
+
   const dateLineHeight = font.heightAtSize(EMAIL_FONT_SIZE);
   const dateLineWidth = font.widthOfTextAtSize(dataEmissao, DATE_FONT_SIZE);
 
+  pdfPage.drawText(dataEmissao, {
+    y: -110.5 - dateLineHeight / 2,
+    size: DATE_FONT_SIZE,
+    font,
+    x: pdfWidth - dateLineWidth - 350,
+  });
 
   const idLineHeight = font.heightAtSize(EMAIL_FONT_SIZE);
   const idLineWidth = font.widthOfTextAtSize(dataEmissao, ID_FONT_SIZE);
 
-  
+  pdfPage.drawText(dataEmissao, {
+    y: -100 - idLineHeight / 2,
+    size: ID_FONT_SIZE,
+    font,
+    x: pdfWidth - idLineWidth - 350,
+  });
 
   const pdfTitle = ["Certificado", name].join(" - ");
   pdf.setTitle(pdfTitle);
